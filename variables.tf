44fa7f0e196a898ed5ee37f06e98fe55d8f6193b
thadeuguimaraes <marciothadeu1984@gmail.com>
2024-01-10 17:40:09 -0300

    feat: modificado o arquivo varaibles.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "thsrematrix-777-thadeuguimaraes"
+  default     = "thsrematrix-guimaraes-cloud"
 }
 
 variable "acl" {

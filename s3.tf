318f7e101b83a77102c33fb76f1faa76b193c793
thadeuguimaraes <marciothadeu1984@gmail.com>
2024-01-10 22:48:09 -0300

    feat: modificado o arquivo variables.tf s3.tf

--- s3.tf
+++ s3.tf
@@ -31,33 +31,34 @@ resource "aws_s3_bucket_acl" "example" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.mybucket.id
-  key          = "index.html"
-  source       = "index.html"
-  acl          = "public-read"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "index.html"
+  source = "index.html"
+  acl = "public-read"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket       = aws_s3_bucket.mybucket.id
-  key          = "error.html"
-  source       = "error.html"
-  acl          = "public-read"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "error.html"
+  source = "error.html"
+  acl = "public-read"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "style" {
-  bucket       = aws_s3_bucket.mybucket.id
-  key          = "style.css"
-  source       = "style.css"
-  acl          = "public-read"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "style.css"
+  source = "style.css"
+  acl = "public-read"
   content_type = "text/css"
 }
 
 resource "aws_s3_object" "script" {
-  bucket       = aws_s3_bucket.mybucket.id
-  key          = "script.js"
-  source       = "script.js"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "script.js"
+  source = "script.js"
+  acl = "public-read"
   content_type = "text/javascript"
 }
 
@@ -71,5 +72,5 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  depends_on = [aws_s3_bucket_acl.example]
+  depends_on = [ aws_s3_bucket_acl.example ]
 }
